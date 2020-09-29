47a9237141b75d5e96f27acb5d1822ee8c88c428
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-09-29 13:39:24 +0900

    修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,13 @@
 class MessagesController < ApplicationController
-  before_action do
-    @conversation = Conversation.find(params[:conversation_id])
-  end
+  before_action :set_conversation
 
-# リファクタリングしてみたが微妙
   def index
     @messages = @conversation.messages.order(:created_at)
     @messages.where.not(user_id: current_user.id).update_all(read: true)
-    unless params[:m] || @messages.length <= 10
-      @messages = Message.where(id: @messages[-10..-1].pluck(:id))
+    unless @messages.length <= 10 || params[:show] == "all"
+      @messages = @messages.last(10)
     end
-    @message = @conversation.messages.build
+    @message = Message.new
   end
 
   # def index
@@ -42,5 +39,9 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:body, :user_id)
+  end
+
+  def set_conversation
+    @conversation = Conversation.find(params[:conversation_id])
   end
 end
